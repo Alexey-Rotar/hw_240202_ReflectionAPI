e6232f26eae5ef013673794976d7a24e12f1994c
alexey.rotar <ar_mail@inbox.ru>
2024-02-04 17:21:43 +0500

    task1 v2

--- src/main/java/ru/geekbrains/junior/lesson2/task1/Animal.java
+++ src/main/java/ru/geekbrains/junior/lesson2/task1/Animal.java
@@ -1,10 +1,10 @@
 package ru.geekbrains.junior.lesson2.task1;
 
 public abstract class Animal {
-    private String name;
-    private int age;
+     private String name;
+     private int age;
 
-    public Animal(String name, int age) {
+    protected Animal(String name, int age) {
         this.name = name;
         this.age = age;
     }
